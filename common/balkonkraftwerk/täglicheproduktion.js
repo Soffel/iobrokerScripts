679e4db84d0e61d79a54a710725ea09f124a0d44
André <Andre@Wagenknecht>
2023-05-19 21:00:03 +0200

    minimierung Db einträge

--- common/balkonkraftwerk/täglicheproduktion.js
+++ common/balkonkraftwerk/täglicheproduktion.js
@@ -14,13 +14,13 @@ async function resetTodayProduced()
 
         if (today > getDateStart(date)) 
         {
+            deleteUnusedData(date);
             setState(BK_PRODUCED_TODAY, 0, true);
             calcAndUpdateProduction(BK_PRODUCED_WEEK, addDays(today, -7));
             calcAndUpdateProduction(BK_PRODUCED_MONTH, new Date(today.getFullYear(),today.getMonth(),1));
             calcAndUpdateProduction(BK_PRODUCED_YEAR, new Date(today.getFullYear(),1,1));
             calcAndUpdateProduction(BK_PRODUCED_TOTAL, null);
-        }
-         
+        }         
     });
 }
 
@@ -29,7 +29,7 @@ async function resetTodayProduced()
 * @param {Date} start
 */
 function calcAndUpdateProduction(name, start) 
-{
+{ 
     new DB(DB_INSTANCE).execute(
         "select " +
             "date((ts/1000),'unixepoch') as realdate, " +
@@ -54,6 +54,60 @@ function calcAndUpdateProduction(name, start)
                     total = total + round(Number(result.result[i].max),2);
                 }
                 setState(name, total, true); 
+            }    
+        });    
+}
+
+/**
+* @param {Date} day
+*/
+function deleteUnusedData(day)
+{
+    var start = getDateStart(day);
+    var end = addDays(start,1);
+
+    var db = new DB(DB_INSTANCE)
+    
+    db.execute(
+        "select " +
+            "ts as time, "+
+            "val as max " +
+        "from ts_number " +
+        "where " +
+            'ts >= ' + start.getTime() + ' AND '+
+            'ts < '+ end.getTime() + ' AND '+
+            'id = (SELECT id FROM datapoints WHERE name="'+ BK_PRODUCED_TODAY + '") '+
+            'order by val DESC '+
+            'LIMIT 1', 
+        function (result) 
+        {
+            if(result.error)
+            {
+                error(result.error)    
+            }
+            else
+            {   
+               var time = result.result[0].time;
+               var max = result.result[0].max;
+
+               db.execute(
+                   "DELETE "+ 
+                   "FROM ts_number "+
+                   "WHERE "+
+                    'ts >= ' + start.getTime() + ' AND '+
+                    'ts < '+ end.getTime() + ' AND '+
+                    'id = (SELECT id FROM datapoints WHERE name="'+ BK_PRODUCED_TODAY + '") and '+
+                    "ts != " + time +"; "+
+                    'COMMIT;', 
+                    function(del)
+                    {
+                        if(del.error)
+                        {
+                            error(result.error)    
+                        } 
+                        else
+                            log("Daten bereinigt MAX: "+max+" at "+ time);
+                    });   
             }    
         });    
 }
